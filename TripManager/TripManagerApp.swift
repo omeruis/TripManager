2804ec88e5eea64a47f011378d9763b4b26b82fc
Omer NAPPORN <nappornomer@gmail.com>
2024-09-22 20:29:53 +0200

    Initial Commit

---
+++ TripManager/TripManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripManagerApp.swift
+//  TripManager
+//
+//  Created by Folivi Omer NAPPORN on 22/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TripManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TripManagerTests/TripManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripManagerTests.swift
+//  TripManagerTests
+//
+//  Created by Folivi Omer NAPPORN on 22/09/2024.
+//
+
+import Testing
+@testable import TripManager
+
+struct TripManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
